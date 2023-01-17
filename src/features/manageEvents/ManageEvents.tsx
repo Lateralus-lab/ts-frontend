6daa0f68d4a5f688b23d122f12eab941ab79c5b0
Eli Krizevski <lateraluz@Elis-MBP.home>
2023-01-17 21:40:07 +0000

    resolved more conflicts

--- src/features/manageEvents/ManageEvents.tsx
+++ src/features/manageEvents/ManageEvents.tsx
@@ -1,95 +1,101 @@
-<<<<<<< HEAD
-import { useGetEventsQuery } from "./manageEventsApiSlice";
-=======
 import { useGetManageEventsQuery } from "./manageEventsApiSlice";
->>>>>>> auth_feature
 import { Link } from "react-router-dom";
 
 import Spinner from "../../components/Spinner";
 
 interface Events {
-  id: number;
-  title: string;
-  release_date: string;
-  runtime: number;
-  mpaa_rating: string;
+    id: number;
+    title: string;
+    release_date: string;
+    runtime: number;
+    mpaa_rating: string;
 }
 
 const ManageEvents = () => {
-  const {
-    data: events,
-    isLoading,
-    isSuccess,
-    isError,
-    error,
-<<<<<<< HEAD
-  } = useGetEventsQuery(null);
-=======
-  } = useGetManageEventsQuery(null);
->>>>>>> auth_feature
+    const {
+        data: events,
+        isLoading,
+        isSuccess,
+        isError,
+        error,
+    } = useGetManageEventsQuery(null);
 
-  let content: any;
+    let content: any;
 
-  if (isLoading) {
-    content = <Spinner />;
-  } else if (isSuccess) {
-    content = (
-      <div className="px-4">
-        <h1 className="title">Manage Events</h1>
+    if (isLoading) {
+        content = <Spinner />;
+    } else if (isSuccess) {
+        content = (
+            <div className="px-4">
+                <h1 className="title">Manage Events</h1>
 
-        <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="py-3 px-6">
-                  Event
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  <div className="flex items-center">Start Date</div>
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  <div className="flex items-center">Runtime</div>
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  <div className="flex items-center">Rating</div>
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  <span className="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {events.map((e: Events) => (
-                <tr
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                  key={e.id}
-                >
-                  <th
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    scope="row"
-                  >
-                    <Link to={`/events/${e.id}`}>{e.title}</Link>
-                  </th>
-                  <td className="py-4 px-6">{e.release_date}</td>
-                  <td className="py-4 px-6">{e.runtime}</td>
-                  <td className="py-4 px-6">{e.mpaa_rating}</td>
-                  <td className="py-4 px-6 text-right">
-                    <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                      <Link to={`/events/${e.id}`}>Edit</Link>
-                    </span>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  } else if (isError) {
-    content = <p>{JSON.stringify(error)}</p>;
-  }
+                <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="py-3 px-6">
+                                    Event
+                                </th>
+                                <th scope="col" className="py-3 px-6">
+                                    <div className="flex items-center">
+                                        Start Date
+                                    </div>
+                                </th>
+                                <th scope="col" className="py-3 px-6">
+                                    <div className="flex items-center">
+                                        Runtime
+                                    </div>
+                                </th>
+                                <th scope="col" className="py-3 px-6">
+                                    <div className="flex items-center">
+                                        Rating
+                                    </div>
+                                </th>
+                                <th scope="col" className="py-3 px-6">
+                                    <span className="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {events.map((e: Events) => (
+                                <tr
+                                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                                    key={e.id}
+                                >
+                                    <th
+                                        className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                        scope="row"
+                                    >
+                                        <Link to={`/events/${e.id}`}>
+                                            {e.title}
+                                        </Link>
+                                    </th>
+                                    <td className="py-4 px-6">
+                                        {e.release_date}
+                                    </td>
+                                    <td className="py-4 px-6">{e.runtime}</td>
+                                    <td className="py-4 px-6">
+                                        {e.mpaa_rating}
+                                    </td>
+                                    <td className="py-4 px-6 text-right">
+                                        <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                            <Link to={`/events/${e.id}`}>
+                                                Edit
+                                            </Link>
+                                        </span>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    } else if (isError) {
+        content = <p>{JSON.stringify(error)}</p>;
+    }
 
-  return content;
+    return content;
 };
 
 export default ManageEvents;
--- src/features/manageEvents/manageEventsApiSlice.ts
+++ src/features/manageEvents/manageEventsApiSlice.ts
@@ -1,21 +1,12 @@
 import { apiSlice } from "../../app/api/apiSlice";
 
 export const manageEventsApiSlice = apiSlice.injectEndpoints({
-<<<<<<< HEAD
-  endpoints: (builder) => ({
-    getEvents: builder.query({
-=======
-  endpoints: (build) => ({
-    getManageEvents: build.query({
->>>>>>> auth_feature
-      query: () => "/admin/manage-events",
-      keepUnusedDataFor: 5,
+    endpoints: (build) => ({
+        getManageEvents: build.query({
+            query: () => "/admin/manage-events",
+            keepUnusedDataFor: 5,
+        }),
     }),
-  }),
 });
 
-<<<<<<< HEAD
-export const { useGetEventsQuery } = manageEventsApiSlice;
-=======
 export const { useGetManageEventsQuery } = manageEventsApiSlice;
->>>>>>> auth_feature
